8bd1d5dda27638cc632abb42f7ce03e3b1c6b349
azkamubrok-win <azka.mubrok@gmail.com>
2026-04-11 17:15:53 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,8 @@
 export default function handler(req, res) {
+  const PRJ = "prj_S16ag2ja7SJMyA3zcArUHz7WLGJH";
+
   res.status(200).json({
-    message: "API bismilah"
+    message: "API bismilah",
+    project_id: PRJ
   });
 }
